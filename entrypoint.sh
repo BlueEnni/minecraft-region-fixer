cfb88296ce28161d5e2f34005d408a11fa0cf474
BlueEnni <Enricobachus@t-online.de>
2020-09-10 15:05:33 +0200

    fixed spacing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 for ((i = 0 ; i < 2 ; i++)); do
-    printf "For Scanning the World enter: 1\n For Scanning and deleting corrupted files enter: 2\n For replacing chunks or regionfiles with backupfiles enter: 3\n For entering a custom command enter: 4\n Exit by entering: 5\n"
-    read input
+    printf "For Scanning the World enter: 1\n For Scanning and deleting corrupted files enter: 2\n For replacing chunks or regionfiles with backupfiles enter: 3\n For entering a custom command enter: 4\n Exit by entering: 5\n";
+    read input;
     if [[ $input == "1" ]] ; then
             echo "python3 regionfixer.py -p 4 /data/world";
 	    $(echo "python3 regionfixer.py -p 4 /data/world");
             i=0;
-	elif [[ $input == "2" ]] ; then
+    elif [[ $input == "2" ]] ; then
             echo "python3 regionfixer.py -p 4 --delete-corrupted /data/world";
 	    $(echo "python3 regionfixer.py -p 4 --delete-corrupted /data/world");
             i=0;
-	elif [[ $input == "3" ]] ; then
+    elif [[ $input == "3" ]] ; then
             echo "Enter the path to the decompressed backup(e.g. /data/FULLBACKUP/decompressed/world)";
             read backuppath;
 	    echo "python3 regionfixer.py -p 4 --replace-wrong --backups $backuppath /data/world";
@@ -25,8 +25,8 @@ for ((i = 0 ; i < 2 ; i++)); do
     elif [[ $input == "5" ]] ; then
 	    echo "Exiting the programm..";
             i=1;
-	else
+    else
 	    echo "Wrong input!";
-        i=0;
+            i=0;
 	fi;
 done
